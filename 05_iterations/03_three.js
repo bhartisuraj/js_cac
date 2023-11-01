bde32ab51ffaa3136aa42e676301e7e824a13bb2
SurajKumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2023-11-01 07:41:52 +0000

    for of loop

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -4,10 +4,10 @@
 // [{}, {}, {}]
 
 /*for of loop for arrays*/
-// const numbers = [1, 2, 3, 4, 5]
-// for(const num of numbers) {
-//   console.log(num);
-// }
+const numbers = ["js", "rb", "py", "java", "cpp"] // for of loop provide array's element as output.
+for(const num of numbers) {
+  console.log(num);
+}
 
 /*for of loop for strings*/
 // let greetings = "hello world!"
@@ -21,12 +21,12 @@
 
 // Maps:- The Map object holds key-value pairs and remembers the original insertion order of the keys. In Maps you can insert only unique values/no duplication. For of loop works on Map().
 
-const map = new Map()
-map.set('IN', 'India')
-map.set('USA', 'United States of America')
-map.set('FR', 'France')
-map.set('IN', 'India')
-console.log(map);
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United States of America')
+// map.set('FR', 'France')
+// map.set('IN', 'India')
+// console.log(map);
 
 // for (const [key, value] of map) {
 //     console.log(key , ":- " , value);
@@ -42,6 +42,6 @@ console.log(map);
 //   console.log(key, ":-", value);
 // }
 
-// FOR in DESCRIPTION       =>     it works for ARRAY and OBJECT but not works on MAP
-// FOR of DESCRIPTION       =>     it works for ARRAY and MAP[ key, value] but not works on OBJECT.
-// FOR each DESCRIPTION     =>     it works for ARRAY, OBJECT and MAP.
+// FOR in       =>     it works for ARRAY(index number) and OBJECT but not works on MAP
+// FOR of       =>     it works for ARRAY(element) and MAP[ key, value] but not works on OBJECT.
+// FOR each     =>     it works for ARRAY, OBJECT and MAP.
